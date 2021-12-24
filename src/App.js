e1ce5d2e1451ef25ca7bf9f1111626fb5c3b9cbe
Alanis Yates <alanisyates@Alaniss-MacBook-Pro.local>
2021-12-24 13:20:37 -0500

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 const App = function () {
   return (
     <div className="App">
       <h1>Hello Real-calc</h1>
-
+      <Sidebar />
     </div>
   );
 };
